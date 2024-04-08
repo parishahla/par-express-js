26584a374d17f3101345b42066f5d610c5c96f4e
pari.dev <parnianshahla2004@gmail.com>
2024-04-08 13:07:59 +0330

    Implemented middleware functionality

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ import ParExpress from "./letsPlayServer.js";
 const app = ParExpress();
 
 app.listen(3000);
+
+app.router.use((req, res, next) => {
+  console.log("middleware 1");
+  next();
+});
+
+app.router.use((req, res, next) => {
+  console.log("middleware 2");
+  next();
+});
+
+app.router.use((req, res, next) => {
+  console.log("middleware 3");
+  next();
+});
+
+app.router.use((req, res, next) => {
+  console.log("middleware 4");
+});
+
+app.router.use((req, res, next) => {
+  console.log("middleware 5");
+});
+
 app.router.get("/par", (req, res) => {
+  console.log("made it to the get");
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello, this is the letsPlay!");
 });
--- letsPlayRouter.js
+++ letsPlayRouter.js
@@ -1,6 +1,8 @@
 class Router {
   constructor() {
     this.routes = {};
+    this.middleware = [];
+    this.index = -1;
   }
 
   get(path, handler) {
@@ -19,12 +21,32 @@ class Router {
     this.routes[path] = handler;
   }
 
+  use(middleware) {
+    this.middleware.push(middleware);
+  }
+
+  //! has to stop
+  executeMiddleware(req, res) {
+    const next = () => {
+      this.index++;
+      if (this.index < this.middleware.length) {
+        this.middleware[this.index](req, res, next);
+      } else {
+        this.index = -1;
+        this.handleRequest(req, res);
+      }
+    };
+    // recursive function
+    next();
+  }
+
+  //? maybe add the method ( as uppercase letters) as well
   handleRequest(req, res) {
-    const { url, method } = req;
+    const { url } = req;
     const handler = this.routes[url];
     if (handler) {
-      // Invoke the handler
       handler(req, res);
+      this.executeMiddleware(req, res);
     } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Not Found");
