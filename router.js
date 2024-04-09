7f1e359ebec4c4285627bb0eb0c088d5a160f59f
pari.dev <parnianshahla2004@gmail.com>
2024-04-09 11:00:57 +0330

    Fixed some bug

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-import queryParse from "./query-parser.js";
+import queryParse from "./query.parser.js";
 class Router {
   constructor() {
     this.routes = {};
@@ -30,11 +30,11 @@ class Router {
   executeMiddleware(req, res) {
     const next = () => {
       this.index++;
-      if (this.index < this.middleware.length) {
+      if (this.index <= this.middleware.length) {
         this.middleware[this.index](req, res, next);
       } else {
         this.index = -1;
-        this.handleRequest(req, res);
+        // this.handleRequest(req, res);
       }
     };
     // recursive function
@@ -45,30 +45,38 @@ class Router {
   handleRequest(req, res) {
     const { url } = req;
     req.query = queryParse(url);
-    res = createResponse(res);
+    res = this.createResponse(res);
+    //!
+    this.executeMiddleware(req, res);
     const handler = this.routes[url];
     if (handler) {
-      this.executeMiddleware(req, res);
+      //!
       handler(req, res);
     } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Not Found");
     }
   }
+
+  createResponse(res) {
+    res.send = (message) => {
+      this.index = -1;
+      res.end(message);
+    };
+
+    res.json = (message) => {
+      res.setHeader("Content-Type", "application/json");
+      this.index = -1;
+      res.end(JSON.stringify(message));
+    };
+
+    res.redirect = (url) => {
+      res.writeHead(302, { Location: `${url}` });
+      this.index = -1;
+      res.end();
+    };
+    return res;
+  }
 }
 
-function createResponse(res) {
-  res.send = (message) => res.end(message);
-
-  res.json = (message) => {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(message));
-  };
-
-  res.redirect = (url) => {
-    res.writeHead(302, { Location: `${url}` });
-    res.end();
-  };
-  return res;
-}
 export default Router;
