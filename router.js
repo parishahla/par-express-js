c3f7076e8b0d138650b1784b0a4b5375d168ecc4
pari.dev <parnianshahla2004@gmail.com>
2024-04-12 10:37:28 +0330

    still needs some modifications

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 import queryParse from "./query.parser.js";
+import FileServer from "./file.server.js";
+
+//! private functions
 class Router {
   constructor() {
     this.routes = {};
@@ -26,33 +29,43 @@ class Router {
     this.middleware.push(middleware);
   }
 
-  //! has to stop
   executeMiddleware(req, res) {
     const next = () => {
       this.index++;
-      if (this.index <= this.middleware.length) {
+      if (this.index < this.middleware.length) {
         this.middleware[this.index](req, res, next);
       } else {
         this.index = -1;
-        // this.handleRequest(req, res);
+        this.handleRequest(req, res);
       }
     };
     // recursive function
     next();
   }
 
-  //? maybe add the method ( as uppercase letters) as well
   handleRequest(req, res) {
     const { url } = req;
-    req.query = queryParse(url);
     res = this.createResponse(res);
-    //!
+
     this.executeMiddleware(req, res);
+
+    if (url.includes("/public")) {
+      this.index = -1;
+      return FileServer.serveFile(req, res);
+    }
+
+    const parsedRoute = queryParse(url);
+
+    //! pattern check
+    if (url.includes("?")) {
+      return res.json(parsedRoute);
+    }
+
     const handler = this.routes[url];
     if (handler) {
-      //!
       handler(req, res);
     } else {
+      this.index = -1;
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Not Found");
     }
@@ -65,14 +78,14 @@ class Router {
     };
 
     res.json = (message) => {
-      res.setHeader("Content-Type", "application/json");
       this.index = -1;
+      res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify(message));
     };
 
     res.redirect = (url) => {
-      res.writeHead(302, { Location: `${url}` });
       this.index = -1;
+      res.writeHead(302, { Location: `${url}` });
       res.end();
     };
     return res;
